d15f2d32bbbbbdeedce01dc7d0861322cb37600d
Sawka01 <8744934@gmail.com>
2020-09-05 15:37:46 +0500

    Initial Commit

---
+++ KHLTeamsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KHLTeamsApp
+//
+//  Created by Macbook on 05.09.2020.
+//  Copyright © 2020 Sawka uz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
